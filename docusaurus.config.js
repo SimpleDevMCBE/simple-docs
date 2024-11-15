fe517e4a13057449057ec1d660c3c9ed6454ac7b
FruitKitty <connorhartey16@icloud.com>
2024-11-15 17:36:56 +0000

    asd

--- docusaurus.config.js
+++ docusaurus.config.js
@@ -27,7 +27,7 @@ const config = {
   // If you aren't using GitHub pages, you don't need these.
   organizationName: 'AmethystDevbyFK', // Usually your GitHub org/user name.
   projectName: 'simple-docs', // Usually your repo name.
-  onBrokenLinks: 'throw',
+  onBrokenLinks: 'warn',
   onBrokenMarkdownLinks: 'warn',
 
   // Even if you don't use internationalization, you can use this field to set
